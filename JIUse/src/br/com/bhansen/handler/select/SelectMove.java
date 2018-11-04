c3acce892daea4c75901734d703b54ec6beae0d2
bruno <bruno@192.168.2.7>
2018-11-04 16:41:26 -0200

    Correção de bug

--- JIUse/src/br/com/bhansen/handler/select/SelectMove.java
+++ JIUse/src/br/com/bhansen/handler/select/SelectMove.java
@@ -1,16 +1,10 @@
 package br.com.bhansen.handler.select;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IType;
-import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.IWorkbenchWindow;
 
-import br.com.bhansen.handler.SelectDlg;
 import br.com.bhansen.metric.AbsMetric;
 import br.com.bhansen.refactory.MoveMethodEvaluator;
 
@@ -31,51 +25,37 @@ public class SelectMove extends SelectionHandler {
 		
 		if(classFrom == null) {
 			
-			MessageDialog.openInformation(window.getShell(), "iMove", "The Metric will be calculated for the openned class!\n\n\n The result dialog will open in a while!");
-			
-			classFrom = getType();
-			
-			MessageDialog.openInformation(window.getShell(), "iMove - Class From Selected!", AbsMetric.getClassName(classFrom) + "\n\n\n Open the 'To Class' and click on the select to move menu again!");
-			
-			return null;
+			try {
+				classFrom = getType();
+				method = AbsMetric.getSignature(getMethod());
+				
+				MessageDialog.openInformation(window.getShell(), "iMove - Method Selected!", method + "\n\n\n Open the 'To Class' and click on the select to move menu again!");
+				
+				return null;
+			} catch (Exception e) {
+				classFrom = null;
+				throw e;
+			}					
+
 		}
 		
 		if(classFrom != null) {
-			
-			MessageDialog.openInformation(window.getShell(), "iMove", "The Metric will be calculated for the openned class!\n\n\n The result dialog will open in a while!");
-			
-			classTo = getType();
-			
-			MessageDialog.openInformation(window.getShell(), "iMove - Class To Selected!", AbsMetric.getClassName(classTo));
-			
-			Set<String> methods = getMethods(classTo);
-			
-			String [] mtds = new String[methods.size()];
-			
-			mtds = methods.toArray(mtds);
-					
-			SelectDlg dlg = new SelectDlg(window.getShell(), "iMove - Choose a method to move!", "Method", mtds);
-			dlg.open();
-			
-			MoveMethodEvaluator evaluator = createEvaluator(classFrom, dlg.getSelection(), classTo, type, metric);
-											
-			MessageDialog.openInformation(window.getShell(), evaluator.shouldMove()? "Move!!!" : "Don't Move!!!", evaluator.toString());
-			
-			classFrom = null;
+			try {
+				
+				classTo = getType();
+				
+				MessageDialog.openInformation(window.getShell(), "iMove - Class To Selected!", AbsMetric.getClassName(classTo) + "\n\n\n The result dialog will open in a while!" );
+								
+				MoveMethodEvaluator evaluator = createEvaluator(classFrom, method, classTo, type, metric);
+												
+				MessageDialog.openInformation(window.getShell(), evaluator.shouldMove()? "Move!!!" : "Don't Move!!!", evaluator.toString());
+				
+			} finally {
+				classFrom = null;
+			}
 		}
 		
 		return null;
-	}
-	
-	private static Set<String> getMethods(IType type) throws JavaModelException {
-		IMethod[] iMethods = type.getMethods();
-		Set<String> methods = new HashSet<>();
-				
-		for (IMethod method : iMethods) {
-			methods.add(AbsMetric.getSignature(method));
-		}
-		
-		return methods;
 	}
 	
 }
