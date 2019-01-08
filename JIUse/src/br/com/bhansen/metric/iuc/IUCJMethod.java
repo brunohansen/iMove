f6fdc387f61dd08a61729e8757ae1168e050f175
bruno <bruno@192.168.2.6>
2019-01-08 16:24:51 -0200

    Permite não adicionar métodos não chamados

--- JIUse/src/br/com/bhansen/metric/iuc/IUCJMethod.java
+++ JIUse/src/br/com/bhansen/metric/iuc/IUCJMethod.java
@@ -12,60 +12,70 @@ import br.com.bhansen.metric.AbsMetric;
 
 public class IUCJMethod extends IUC {
 	
+	private final static boolean ADD_NOT_CALLED = true;
+
 	private Set<String> method;
 	private boolean publicMethod;
-	
+
 	public IUCJMethod(IType type, String method) throws Exception {
 		super(type);
-		
+
 		IMethod[] iMethods = type.getMethods();
-		
-		if(isMovedMethod(method)) {
+
+		if (isMovedMethod(method)) {
 			IMethod movedMethod = null;
-			
+
 			for (IMethod iMethod : iMethods) {
-				
-				if(isMovedMethod(iMethod, method)) {
+
+				if (isMovedMethod(iMethod, method)) {
 					movedMethod = iMethod;
 					this.publicMethod = Flags.isPublic(iMethod.getFlags());
 				} else {
-					if(getMethods().put(getSignature(iMethod), getCallers(iMethod)) != null) {
-						System.out.println("Method " + getSignature(iMethod) + " colision!");
-					};	
-				}
-				
-			}
-			
-//			String original = getOriginalMethod(movedMethod);
-//			
-//			//Does not make sense compare itself
-//			if(original != null) {
-//				this.method = getMethods().remove(original);
-//			} else {
-				this.method = getCallers(movedMethod);
-//			}
-			
-		} else {
-			for (IMethod iMethod : iMethods) {
-				
-				if(isMethod(iMethod, method)) {
-					this.method = getCallers(iMethod);
-					this.publicMethod = Flags.isPublic(iMethod.getFlags());
-				} else {
-					if(getMethods().put(getSignature(iMethod), getCallers(iMethod)) != null) {
-						System.out.println("Method " + getSignature(iMethod) + " colision!");
-					};
+					Set<String> callers = getCallers(iMethod);
+
+					if ((ADD_NOT_CALLED) || (callers.size() > 0)) {
+						if (getMethods().put(getSignature(iMethod), callers) != null) {
+							System.out.println("Method " + getSignature(iMethod) + " colision!");
+						}
+					}
 				}
 
 			}
-		}		
+
+			// String original = getOriginalMethod(movedMethod);
+			//
+			// //Does not make sense compare itself
+			// if(original != null) {
+			// this.method = getMethods().remove(original);
+			// } else {
+			this.method = getCallers(movedMethod);
+			// }
+
+		} else {
+			for (IMethod iMethod : iMethods) {
+
+				if (isMethod(iMethod, method)) {
+					this.method = getCallers(iMethod);
+					this.publicMethod = Flags.isPublic(iMethod.getFlags());
+				} else {
+					Set<String> callers = getCallers(iMethod);
+
+					if ((ADD_NOT_CALLED) || (callers.size() > 0)) {
+						if (getMethods().put(getSignature(iMethod), callers) != null) {
+							System.out.println("Method " + getSignature(iMethod) + " colision!");
+						}
+					}
+				}
+
+			}
+		}
 	}
-	
+
 	@Override
 	public boolean isPublicMethod() {
 		return this.publicMethod;
 	}
-	
+
 	@Override
 	public boolean hasNoCaller() {
 		return this.method.isEmpty();
@@ -73,31 +83,29 @@ public class IUCJMethod extends IUC {
 
 	@Override
 	public boolean isCalledOnlyBy(IType type) {
-		return (! this.method.isEmpty()) && (this.method.size() == 1) && (this.method.contains(getClassName(type)));
+		return (!this.method.isEmpty()) && (this.method.size() == 1) && (this.method.contains(getClassName(type)));
 	}
-	
+
 	@Override
 	public double getMetric() throws Exception {
 		return getMetric(this.method, this.getMethods());
 	}
-	
+
 	public static double getMetric(Set<String> method, Map<String, Set<String>> methods) {
 		double metric = 0;
-		
-		if(methods.size() == 0)
+
+		if (methods.size() == 0)
 			return 0;
-		
+
 		for (Entry<String, Set<String>> entry : methods.entrySet()) {
-			
+
 			metric += AbsMetric.howMuchIntersect(method, entry.getValue());
-						
+
 		}
-		
+
 		metric = metric / methods.size();
 
 		return metric;
 	}
-	
-
 
 }
