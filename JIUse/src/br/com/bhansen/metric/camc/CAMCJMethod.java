622b7d4897298fee4d47704e1052783106b9932c
bruno <bruno@192.168.2.6>
2019-01-16 13:32:18 -0200

    Preparando para usar niveis de visibilidade

--- JIUse/src/br/com/bhansen/metric/camc/CAMCJMethod.java
+++ JIUse/src/br/com/bhansen/metric/camc/CAMCJMethod.java
@@ -4,6 +4,7 @@ import java.util.Map.Entry;
 import java.util.Set;
 
 import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.JavaModelException;
 
 import br.com.bhansen.metric.AbsMetric;
 import br.com.bhansen.metric.DeclarationMetric;
@@ -13,18 +14,60 @@ import br.com.bhansen.utils.Type;
 
 public class CAMCJMethod extends DeclarationMetric {
 
-	Set<String> method;
+	private Set<String> method;
 
 	public CAMCJMethod(Type type, String method, String parameter) throws Exception {
 		super(type);
 
+		dontUseVisibilityLevels(type, method, parameter);
+	}
+	
+	private void useVisibilityLevels(Type type, String method, String parameter) throws JavaModelException, Exception {
+		
+		Method tMethod = type.getMethod(method);
+		
+		this.method = tMethod.getMethodWithParameters(parameter).getParameters();
+		
 		IMethod[] iMethods = type.getIType().getMethods();
 
 		for (IMethod iMethod : iMethods) {
 			
 			Method m = new Method(iMethod);
 
-			if (m.isMethod(method) || m.isMovedMethod(method)) {
+			if (! m.isMethod(method)) {
+								
+				// Dont add constructor
+				if (m.isConstructor())
+					continue;
+				
+				// Dont add private
+//				if (m.isPrivate())
+//					continue;
+				
+				// Add only public
+				if(! m.isPublic())
+					continue;
+				
+				MethodWithParameters mp = m.getMethodWithParameters(parameter);
+				
+				// Dont add zero parameters
+//				if(! mp.hasParameter())
+//					continue;
+
+				getMethods().put(mp.getSignature(), mp.getParameters());
+			}
+
+		}
+	}
+
+	private void dontUseVisibilityLevels(Type type, String method, String parameter) throws JavaModelException, Exception {
+		IMethod[] iMethods = type.getIType().getMethods();
+
+		for (IMethod iMethod : iMethods) {
+			
+			Method m = new Method(iMethod);
+
+			if (m.isMethod(method)) {
 				this.method = m.getMethodWithParameters(parameter).getParameters();
 			} else {
 				
