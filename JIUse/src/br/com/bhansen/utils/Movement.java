1bce31733715ab275af1ec007dbde555af611200
bruno <bruno@DESKTOP-U1BEN5U>
2019-03-09 14:50:48 -0300

    Correcting bug Methods returning generics or array

--- JIUse/src/br/com/bhansen/utils/Movement.java
+++ JIUse/src/br/com/bhansen/utils/Movement.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class Movement {
 	
-	final static Pattern linePattern = Pattern.compile("(([a-z|A-Z|0-9|_|$]*\\.){1,}[a-z|A-Z|0-9|_|$]{1,}::[a-z|A-Z|0-9|_|$]{1,}\\(.*?\\):([a-z|A-Z|0-9|_|$]*\\.)*[a-z|A-Z|0-9|_|$]{1,}).+?(([a-z|A-Z|0-9|_|$]*\\.){1,}[a-z|A-Z|0-9|_|$]{1,})");
+	final static Pattern linePattern = Pattern.compile("(([a-z|A-Z|0-9|_|$]*\\.){1,}[a-z|A-Z|0-9|_|$]{1,}::[a-z|A-Z|0-9|_|$]{1,}\\(.*?\\):([a-z|A-Z|0-9|_|$]*\\.)*[a-z|A-Z|0-9|_|$]{1,}(.+[\\]+>+]){0,1}).+?(([a-z|A-Z|0-9|_|$]*\\.){1,}[a-z|A-Z|0-9|_|$]{1,})");
 	
 	public final static int SOURCE_CLASS = 0;
 	public final static int METHOD = 1;
@@ -17,7 +17,7 @@ public class Movement {
 		if(matcher.find()) {
 			String [] method = matcher.group(1).split("::");
 	
-			return new String[]{method[0], method[1], matcher.group(4)};
+			return new String[]{method[0], method[1], matcher.group(5)};
 		} else {
 			throw new Exception("Invalid syntax: " + movement);
 		}
@@ -49,12 +49,13 @@ public class Movement {
 	
 	public static void main(String[] args) {
 		String text = "com.mvnforum.admin.GeneralAdminTasksWebHandler::processMailTemplate(com.mvnforum.db.MemberBean, java.lang.String):java.lang.String	com.mvnforum.db.MemberBean";
-		Pattern sc = Pattern.compile("(([a-z|A-Z|0-9|_|$]*\\.){1,}[a-z|A-Z|0-9|_|$]{1,}::[a-z|A-Z|0-9|_|$]{1,}\\(.*\\):([a-z|A-Z|0-9|_|$]*\\.)*[a-z|A-Z|0-9|_|$]{1,}).?(([a-z|A-Z|0-9|_|$]*\\.)*[a-z|A-Z|0-9|_|$]{1,})");
+		//String text = "org.junit.runners.model.TestClass::getAllArguments(Assignments, boolean):Object[]	org.junit.experimental.theories.internal.Assignments";
+		//String text = "org.apache.tapestry5.integration.app1.data.Track::getTracks(GridSetDemo):Set<Track>	org.apache.tapestry5.integration.app1.pages.GridSetDemo";
 		
-		Matcher mSC = sc.matcher(text);
+		Matcher mSC = linePattern.matcher(text);
 		mSC.find();
 		
-		System.out.println(mSC.group(1) + "\t" + mSC.group(4));
+		System.out.println(mSC.group(1) + "|\t|" + mSC.group(5));
 				
 	}
 
