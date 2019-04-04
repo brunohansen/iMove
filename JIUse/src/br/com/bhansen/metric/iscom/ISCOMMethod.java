a907f2df76f86199e710edfd1690d0d859608def
bruno <bruno@DESKTOP-U1BEN5U>
2019-04-03 23:00:14 -0300

    Nova metrica 2019

---
+++ JIUse/src/br/com/bhansen/metric/iscom/ISCOMMethod.java
@@ -0,0 +1,48 @@
+package br.com.bhansen.metric.iscom;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+import br.com.bhansen.metric.DeclarationMetricMethod;
+import br.com.bhansen.utils.Type;
+
+public class ISCOMMethod extends DeclarationMetricMethod {
+
+	public ISCOMMethod(Type type, String method, String parameter, IProgressMonitor monitor) throws Exception {
+		super(type, method, parameter, monitor);
+	}
+
+	public static double c(Set<String> i, Set<String> j) {
+		Set<String> intersection = new HashSet<>(i);
+		intersection.retainAll(j);
+		
+		return (2 * intersection.size()) / (i.size() + j.size());
+	}
+	
+	public static double w(Set<String> i, Set<String> j, int a) {
+		Set<String> union = new HashSet<>(i);
+		union.addAll(j);
+		
+		return union.size() / a;
+	}
+
+	@Override
+	public double getMetric(Set<String> mi, Map<String, Set<String>> methods) {
+		return getMetric(mi, methods, getParams().size());
+	}
+	
+	public static double getMetric(Set<String> mi, Map<String, Set<String>> methods, int a) {
+		double r = 0;
+				
+		for (Entry<String, Set<String>> mj : methods.entrySet()) {
+			r = r + (c(mi, mj.getValue()) * w(mi, mj.getValue(), a));
+		}
+		
+		return r / methods.size();
+	}
+
+}
---
+++ JIUse/src/br/com/bhansen/metric/iscom/UISCOMMethod.java
@@ -0,0 +1,22 @@
+package br.com.bhansen.metric.iscom;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+import br.com.bhansen.metric.UsageMetricMethod;
+import br.com.bhansen.utils.Type;
+
+public class UISCOMMethod extends UsageMetricMethod {
+
+	public UISCOMMethod(Type type, String method, IProgressMonitor monitor) throws Exception {
+		super(type, method, monitor);
+	}
+
+	@Override
+	public double getMetric(Set<String> mi, Map<String, Set<String>> methods) {
+		return ISCOMMethod.getMetric(mi, methods, getClients().size());
+	}
+
+}
