2a13b2a901fa4239b157d2dca908b1e723b053cf
bruno <bruno@DESKTOP-U1BEN5U>
2019-04-10 20:49:39 -0300

    New metric

---
+++ JIUse/src/br/com/bhansen/metric/d3c2i/D3C2iClass.java
@@ -0,0 +1,52 @@
+package br.com.bhansen.metric.d3c2i;
+
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+import br.com.bhansen.jdt.Type;
+import br.com.bhansen.metric.AbsMetric;
+import br.com.bhansen.metric.DeclarationMetricClass;
+import br.com.bhansen.metric.nhd.NHDClass;
+
+public class D3C2iClass extends DeclarationMetricClass {
+
+	protected D3C2iClass(Type type, IProgressMonitor monitor) throws Exception {
+		super(type, monitor);
+	}
+
+	@Override
+	public double getMetric() throws Exception {
+		return getMetric(getMethods());
+	}
+	
+	public static double getMetric(Map<String, Set<String>> methods) throws Exception {
+		double k = methods.size();
+		double l = AbsMetric.uniqueValues(methods).size();
+				
+		return ((k * (k - 1) * mmac(methods)) + (l * (l - 1) * aac(methods)) + (2 * k * l * amc(methods))) / ((k * (k - 1)) + (l * (l - 1)) + (2 * k * l));
+	}
+	
+	public static double mmac(Map<String, Set<String>> methods) {
+		return NHDClass.nhdClass(methods, AbsMetric.uniqueValues(methods), NHDClass.NHD);
+	}
+	
+	public static double aac(Map<String, Set<String>> methods) {
+		Map<String, Set<String>> parameters = AbsMetric.transpose(methods);
+		
+		return NHDClass.nhdClass(parameters, AbsMetric.uniqueValues(parameters), NHDClass.NHD);
+	}
+	
+	public static double amc(Map<String, Set<String>> methods) {
+		double p = 0;
+		
+		for (Entry<String, Set<String>> method : methods.entrySet()) {
+			p = p + method.getValue().size();
+		}
+		
+		return p / (double) (methods.size() * methods.size());
+	}
+
+}
