235c58d2434ae1fbf2242bccbf582db9f7c32adb
bruno <bruno@DESKTOP-U1BEN5U>
2019-04-20 14:10:18 -0300

    update

--- JIUse/test/br/com/bhansen/metric/MethodCorrelationTest.java
+++ JIUse/test/br/com/bhansen/metric/MethodCorrelationTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 
 public class MethodCorrelationTest extends MetricTest {
 	
-//	@Test
+	@Test
 	public void testExampleAClass() {
 		Map<String, Set<String>> classA = new HashMap<>();
 		
@@ -90,7 +90,7 @@ public class MethodCorrelationTest extends MetricTest {
 		print(classB);
 	}
 	
-	@Test
+//	@Test
 	public void testExampleAMethod() {
 		Map<String, Set<String>> classA = new HashMap<>();
 		
@@ -113,41 +113,20 @@ public class MethodCorrelationTest extends MetricTest {
 	public void testExampleBMethod() {
 		Map<String, Set<String>> classA = new HashMap<>();
 		
-		System.out.println("\n Example B - m4 na ClasseX \n");
-		
 		classA.put("m1", new HashSet<>(Arrays.asList("p1", "p2", "p3")));
 		classA.put("m2", new HashSet<>(Arrays.asList("p1", "p2", "p3")));
-		classA.put("m4", new HashSet<>(Arrays.asList("p1")));
 		
-		System.out.println("\n ClasseX \n");
-		print(classA);
+		System.out.println("\n Example B - m4 na ClasseX \n");
+		print(new HashSet<>(Arrays.asList("p1")), classA);
 		
 		Map<String, Set<String>> classB = new HashMap<>();
 
 		classB.put("m1", new HashSet<>(Arrays.asList("p4", "p5", "p6")));
 		classB.put("m2", new HashSet<>(Arrays.asList("p4", "p7")));
 		classB.put("m3", new HashSet<>(Arrays.asList("p5", "p7")));
-		
-		System.out.println("\n ClasseY \n");
-		print(classB);
-		
-		System.out.println("\n Example B - m4 na ClasseY \n");
-		
-		classA.clear();
-		classA.put("m1", new HashSet<>(Arrays.asList("p1", "p2", "p3")));
-		classA.put("m2", new HashSet<>(Arrays.asList("p1", "p2", "p3")));
-		
-		System.out.println("\n ClasseX \n");
-		print(classA);
 
-		classB.clear();
-		classB.put("m1", new HashSet<>(Arrays.asList("p4", "p5", "p6")));
-		classB.put("m2", new HashSet<>(Arrays.asList("p4", "p7")));
-		classB.put("m3", new HashSet<>(Arrays.asList("p5", "p7")));
-		classB.put("m4", new HashSet<>(Arrays.asList("p1")));
-		
-		System.out.println("\n ClasseY \n");
-		print(classB);
+		System.out.println("\n Example B - m4 na ClasseY \n");
+		print(new HashSet<>(Arrays.asList("p1")), classB);
 	}
 
 }
