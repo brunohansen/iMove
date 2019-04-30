81213e70c0edaccfe76f88a623fcd96b20212676
bruno <bruno@DESKTOP-U1BEN5U>
2019-04-30 16:44:51 -0300

    Adding collections

--- JIUse/src/br/com/bhansen/jdt/ParameterHelper.java
+++ JIUse/src/br/com/bhansen/jdt/ParameterHelper.java
@@ -30,18 +30,33 @@ public class ParameterHelper {
 		primitives.add("boolean");
 		primitives.add("Boolean");
 		primitives.add("String");
-		primitives.add("void");
-		
+		primitives.add("void");		
+
+		collections.add("Collection");
 		collections.add("Set");
 		collections.add("List");
-		collections.add("Map");
 		collections.add("Queue");
 		collections.add("Deque");
+		collections.add("Map");
+		collections.add("SortedSet");
+		collections.add("SortedMap");
+		collections.add("NavigableSet");
+		collections.add("NavigableMap");
+		collections.add("BlockingQueue");
+		collections.add("TransferQueue");
+		collections.add("BlockingDeque");
+		collections.add("ConcurrentMap");
+		collections.add("ConcurrentNavigableMap");
 		collections.add("HashSet");
+		collections.add("TreeSet");
+		collections.add("LinkedHashSet");
 		collections.add("ArrayList");
-		collections.add("HashMap");
-		collections.add("LinkedList");
 		collections.add("ArrayDeque");
+		collections.add("LinkedList");
+		collections.add("PriorityQueue");
+		collections.add("HashMap");
+		collections.add("TreeMap");
+		collections.add("LinkedHashMap");
 		
 	}
 
